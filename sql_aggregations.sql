3eda76d5399284b90f6807b85d56f0026c00fb68
bondxue <menghengxue@gmail.com>
2021-03-16 00:33:57 -0400

    update sql joins

--- sql_aggregations.sql
+++ sql_aggregations.sql
@@ -39,6 +39,61 @@ SELECT AVG(standard_qty),
        AVG(poster_amt_usd)
 FROM orders;
 
+---GROUP BY---
+SELECT a.name, MIN(o.occurred_at) ealiest_occurred_at
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name
+ORDER BY ealiest_occurred_at
+LIMIT 1;
+
+SELECT a.name, SUM(total_amt_usd)
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name;
+
+SELECT a.name, w.occurred_at, w.channel
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+ORDER BY w.occurred_at DESC
+LIMIT 1;
+
+SELECT w.channel, COUNT(w.occurred_at)
+FROM web_events w
+GROUP BY w.channel;
+
+SELECT a.primary_poc, w.occurred_at
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+ORDER BY w.occurred_at
+LIMIT 1;
+
+SELECT a.name, MIN(total_amt_usd) min_total_amt_usd
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name
+ORDER BY min_total_amt_usd;
+
+SELECT COUNT(r.id) num_sales_reps, r.name
+FROM region r
+JOIN sales_reps sr
+ON r.id = sr.region_id
+GROUP BY  r.name
+ORDER BY num_sales_reps;
+
+SELECT
+
+
+
+
+
+
+
 
 
 
